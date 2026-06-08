56f75ff3dbef4aa8339114282aebcc5922c747de
EXTRAGLIT <107995357+EXTRAGLIT@users.noreply.github.com>
2026-06-07 22:27:36 -0700

    Add files via upload

---
+++ studentinfo.js
@@ -0,0 +1,10 @@
+const studentName = "Dominick Warner";
+const major = "Computer Science";
+const email = "killerbeelol23@yahoo.com";
+const graduationDate = "May 2027";
+
+document.getElementById("info").innerHTML =
+    "<p><strong>Name:</strong> " + studentName + "</p>" +
+    "<p><strong>Major:</strong> " + major + "</p>" +
+    "<p><strong>Email:</strong> " + email + "</p>" +
+    "<p><strong>Expected Graduation:</strong> " + graduationDate + "</p>";
